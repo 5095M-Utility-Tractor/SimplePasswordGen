1ef375f80ed59611a74076b29ea757d5244b97b6
Augie <129338000+5095M-Utility-Tractor@users.noreply.github.com>
2024-02-28 14:03:12 -0800

    Added more symbols
    More symbols added, as well as bare bones function to copy password to clipboard

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# Create 4 arrays Alphabet cap and lower case, Numbers and symbols Done
-# Function that randomly picks an array then picks and adds it to password
 import random
 import secrets
-
-from tkinter import Tk
+import pyperclip
 
 AlphabetCap = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X", "Y", "Z"]
 AlphabetLower = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x", "y", "z"]
 Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+"]
+Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "'", ",", ".", "/", "?", "<", ">", "|", "[", "]", "{", "}", "`", "~"]
 password = ""
 
-def generatePassword(password):
-    RandNum = random.randint(0,3)
+
+def generatePassword(password): # Function that generates the password, picking randomly from each array
+    RandNum = random.randint(0, 3)
     if RandNum == 0:
         item = secrets.choice(AlphabetCap)
         password += item
@@ -29,9 +29,17 @@ def generatePassword(password):
         password += item
     return password
 
-PassLength = int(input("Enter Password Length: "))
+
+PassLength = int(input("Enter Password Length: ")) # Determines how long the password will be
 
 for x in range(PassLength):
-   password = generatePassword(password)
+    password = generatePassword(password)
 
 print(password)
+answer = input("Would you like to copy password to clipboard Y or N: ")
+if answer == "Y":
+    pyperclip.copy(password) # Copies password to clipboard
+    print(password, " Has been copied to clipboard")
+elif answer == "N":
+    print("Exit")
+
