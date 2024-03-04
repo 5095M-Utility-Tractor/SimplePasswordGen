bd35c90408190845625e410c6927999256c98440
Augie <129338000+5095M-Utility-Tractor@users.noreply.github.com>
2024-03-04 14:17:13 -0800

    Added Loop to regenrate passwords

--- main.py
+++ main.py
@@ -9,37 +9,40 @@ AlphabetLower = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
     "x", "y", "z"]
 Numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "'", ",", ".", "/", "?", "<", ">", "|", "[", "]", "{", "}", "`", "~"]
+Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "'", ",", ".", "/", "?", "<", ">", "|", "[",
+           "]", "{", "}", "`", "~"]
 password = ""
+numRange = [1, 2, 3, 0]
 
 
-def generatePassword(password): # Function that generates the password, picking randomly from each array
-    RandNum = random.randint(0, 3)
-    if RandNum == 0:
-        item = secrets.choice(AlphabetCap)
-        password += item
-    elif RandNum == 1:
-        item = secrets.choice(AlphabetLower)
-        password += item
-    elif RandNum == 2:
-        item = secrets.choice(Numbers)
-        password += item
+def generatePassword(password):  # Function that generates the password, picking randomly from each array
+    randNum = secrets.choice(numRange)
+    if randNum == 0:
+        password += "".join(secrets.choice(AlphabetCap))
+    elif randNum == 1:
+        password += "".join(secrets.choice(AlphabetLower))
+    elif randNum == 2:
+        password += "".join(secrets.choice(Numbers))
     else:
-        item = secrets.choice(Symbols)
-        password += item
+        password += "".join(secrets.choice(Symbols))
     return password
 
 
-PassLength = int(input("Enter Password Length: ")) # Determines how long the password will be
+while True:
+    PassLength = int(input("Enter Password Length: "))  # Determines how long the password will be
 
-for x in range(PassLength):
-    password = generatePassword(password)
+    for x in range(PassLength):
+        password = generatePassword(password)
 
-print(password)
-answer = input("Would you like to copy password to clipboard Y or N: ")
-if answer == "Y":
-    pyperclip.copy(password) # Copies password to clipboard
-    print(password, " Has been copied to clipboard")
-elif answer == "N":
-    print("Exit")
-
+    print(password)
+    answer = input("Would you like to copy password to clipboard Y or N: ")
+    if answer in ("Y", "y"):
+        pyperclip.copy(password)  # Copies password to clipboard
+        print(password, " Has been copied to clipboard")
+    elif answer in ("N", "n"):
+        answer = input("N to Exit or Y to regenerate password: ")
+        if answer in ("N", "n"):
+            print("Exit")
+            break
+        else:
+            continue
